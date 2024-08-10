7463cb1cc7026f4f073d6a8404e72141c3b19e28
nhoxha95 <neritanhoxha2018@gmail.com>
2024-08-10 20:58:54 +0100

    just commented any misunderstandings

--- code-snapshots/02-essentials/36-localstorage/src/app/shared/card/card.component.ts
+++ code-snapshots/02-essentials/36-localstorage/src/app/shared/card/card.component.ts
@@ -10,3 +10,4 @@ import { Component } from '@angular/core';
 export class CardComponent {
 
 }
+ 
--- code-snapshots/02-essentials/36-localstorage/src/app/tasks/new-task/new-task.component.ts
+++ code-snapshots/02-essentials/36-localstorage/src/app/tasks/new-task/new-task.component.ts
@@ -1,7 +1,6 @@
 import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { type NewTaskData } from '../task/task.model';
 import { TasksService } from '../tasks.service';
 
 @Component({
@@ -12,19 +11,24 @@ import { TasksService } from '../tasks.service';
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
-  @Input({ required: true }) userId!: string;
-  @Output() close = new EventEmitter<void>();
+  @Input({ required: true }) userId!: string; // marim user id nga app-tasks
+  // eshte void sepse nuk po percjell te dhena, thjesht po percjellim sinjalin
+  @Output() close = new EventEmitter<void>(); // sinjalizojme (close)="onCloseAddTask()" ne app-tasks duke mbullur dialogun
+
+  // specifikoj ca vlera boshe
   enteredTitle = '';
   enteredSummary = '';
   enteredDate = '';
-  private tasksService = inject(TasksService);
+
+  // constructor(private tasksService: TasksService) {}
+  private tasksService = inject(TasksService); // si inject si constructor funksjoni eshte i njejte
 
   onCancel() {
     this.close.emit();
   }
 
-  onSubmit() {
-    this.tasksService.addTask(
+  onSubmit() { 
+    this.tasksService.addTask( // krijojme vlera te reja duke mbyllur dialog
       {
         title: this.enteredTitle,
         summary: this.enteredSummary,
