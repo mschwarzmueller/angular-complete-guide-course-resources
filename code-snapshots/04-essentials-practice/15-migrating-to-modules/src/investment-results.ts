0983ae3d5cd9ff800454c5da5f8131ce0e439bb5
nhoxha95 <neritanhoxha2018@gmail.com>
2024-08-11 22:40:24 +0100

    just commentaion and test

--- code-snapshots/04-essentials-practice/15-migrating-to-modules/src/investment-results.ts
+++ code-snapshots/04-essentials-practice/15-migrating-to-modules/src/investment-results.ts
@@ -2,25 +2,25 @@
 // e.g., integrate it into a service or component
 // You may need to tweak it, depending on where and how you use it
 
-function calculateInvestmentResults() {
-  const annualData = [];
-  let investmentValue = initialInvestment;
+// function calculateInvestmentResults() {
+//   const annualData = [];
+//   let investmentValue = initialInvestment;
 
-  for (let i = 0; i < duration; i++) {
-    const year = i + 1;
-    const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-    investmentValue += interestEarnedInYear + annualInvestment;
-    const totalInterest =
-      investmentValue - annualInvestment * year - initialInvestment;
-    annualData.push({
-      year: year,
-      interest: interestEarnedInYear,
-      valueEndOfYear: investmentValue,
-      annualInvestment: annualInvestment,
-      totalInterest: totalInterest,
-      totalAmountInvested: initialInvestment + annualInvestment * year,
-    });
-  }
+//   for (let i = 0; i < duration; i++) {
+//     const year = i + 1;
+//     const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+//     investmentValue += interestEarnedInYear + annualInvestment;
+//     const totalInterest =
+//       investmentValue - annualInvestment * year - initialInvestment;
+//     annualData.push({
+//       year: year,
+//       interest: interestEarnedInYear,
+//       valueEndOfYear: investmentValue,
+//       annualInvestment: annualInvestment,
+//       totalInterest: totalInterest,
+//       totalAmountInvested: initialInvestment + annualInvestment * year,
+//     });
+//   }
 
-  return annualData;
-}
+//   return annualData;
+// }
