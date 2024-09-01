70432a21ed6c36dd2ea6305dae0a21f1242dee72
nhoxha95 <neritanhoxha2018@gmail.com>
2024-09-01 19:07:09 +0100

    test and comment

--- code-snapshots/08-pipes-deep-dive/06-finished/src/app/sort.pipe.ts
+++ code-snapshots/08-pipes-deep-dive/06-finished/src/app/sort.pipe.ts
@@ -6,15 +6,23 @@ import { Pipe, PipeTransform } from '@angular/core';
   pure: false
 })
 export class SortPipe implements PipeTransform {
+
+  // value = [25, 37, 19, -4, 28, 21, 19, 28, 33, 31, 9, 11, 5, -12, -5];
+  // if direction is 'asc': value = [-12, -5, -4, 5, 9, 11, 19, 19, 21, 25, 28, 28, 31, 33, 37]
+  // if direction is 'desc': value = [37, 33, 31, 28, 28, 25, 21, 19, 19, 11, 9, 5, -4, -5, -12]
+
   transform(value: string[] | number[], direction: 'asc' | 'desc' = 'asc') {
-    const sorted = [...value];
-    sorted.sort((a, b) => {
+    const sorted = [...value]; // Javascript Spread Operation, na mundeson copy - paste ne -> sorted
+    sorted.sort((a, b) => { 
       if (direction === 'asc') {
-        return a > b ? 1 : -1;
+        // nga më i vogli te më i madhi
+        return a > b ? 1 : -1; // The function returns 1 if a > b and -1 otherwise.
       } else {
-        return a > b ? -1 : 1;
+        // nga më i madhi te më i vogli
+        return a > b ? -1 : 1; // The function returns -1 if a > b and 1
       }
     });
     return sorted;
   }
 }
+ 
--- code-snapshots/08-pipes-deep-dive/06-finished/src/app/temperature.pipe.ts
+++ code-snapshots/08-pipes-deep-dive/06-finished/src/app/temperature.pipe.ts
@@ -5,41 +5,44 @@ import { Pipe, PipeTransform } from '@angular/core';
   standalone: true,
 })
 export class TemperaturePipe implements PipeTransform {
-  transform(
-    value: string | number | null,
+  transform( // marim tre vlera 
+    value: string | number | null, 
     inputType: 'cel' | 'fah',
     outputType?: 'cel' | 'fah'
   ) {
-    if (!value) {
+    if (!value) { // nese vlerat eshte null, return, sepse e kemi shtur null brenda vleres: value: string | number | null
       return value;
     }
 
     let val: number;
 
-    if (typeof value === 'string') {
-      val = parseFloat(value);
-    } else {
+    if (typeof value === 'string') { // kontrollojme nese vlera eshte string.
+      // convert a string or other value into a floating-point number
+      val = parseFloat(value); // parseFloat("123.45"); // returns 123.45
+    } else { // nese vlera eshte number val = value
       val = value;
     }
 
     let outputTemp: number;
 
     if (inputType === 'cel' && outputType === 'fah') {
-      outputTemp = val * (9 / 5) + 32;
+      outputTemp = val * (9 / 5) + 32; // convert celsius to fahrenheit
     } else if (inputType === 'fah' && outputType === 'cel') {
-      outputTemp = (val - 32) * (5 / 9);
+      outputTemp = (val - 32) * (5 / 9); // convert fahrenheit to celsius 
     } else {
       outputTemp = val;
-    }
+    } 
 
     let symbol: '°C' | '°F';
 
-    if (!outputType) {
-      symbol = inputType === 'cel' ? '°C' : '°F';
+    if (!outputType) { // duke ke qen se inputType eshte gjithmon dhe outputType jo, bejme kushtet e symbol
+      symbol = inputType === 'cel' ? '°C' : '°F'; // if inputType === 'cel -> °C else -> °F
     } else {
       symbol = outputType === 'cel' ? '°C' : '°F';
     }
 
-    return `${outputTemp.toFixed(2)} ${symbol}`;
+    // outputTemp == 1.777777777777
+    // outputTemp.toFixed(2) = 1.77
+    return `${outputTemp.toFixed(2)} ${symbol}`; 
   }
 }
